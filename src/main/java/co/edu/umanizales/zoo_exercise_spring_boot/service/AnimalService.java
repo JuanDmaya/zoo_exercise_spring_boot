999f7843b2541995ca6f507981b38b56cf8948fc
Jdiegomaya <juandiegomayamorales@gmail.com>
2024-10-24 14:49:56 -0500

    quité comentarios

--- src/main/java/co/edu/umanizales/zoo_exercise_spring_boot/service/AnimalService.java
+++ src/main/java/co/edu/umanizales/zoo_exercise_spring_boot/service/AnimalService.java
@@ -11,18 +11,18 @@ import java.util.Optional;
 public class AnimalService {
     private List<Animal> animales = new ArrayList<>();
 
-    // Agregar animal
+
     public Animal agregarAnimal(Animal animal) {
         animales.add(animal);
         return animal;
     }
 
-    // Obtener todos los animales
+ 
     public List<Animal> obtenerTodosLosAnimales() {
         return animales;
     }
 
-    // Actualizar un animal por nombre
+
     public Optional<Animal> actualizarAnimal(String nombre, Animal nuevoAnimal) {
         for (int i = 0; i < animales.size(); i++) {
             if (animales.get(i).getNombre().equals(nombre)) {
@@ -33,7 +33,7 @@ public class AnimalService {
         return Optional.empty();
     }
 
-    // Eliminar un animal por nombre
+
     public boolean eliminarAnimal(String nombre) {
         return animales.removeIf(animal -> animal.getNombre().equals(nombre));
     }
